edc6cec04ee5c15408fdcaf82e975d8c1955a030
Sergio Borbone <sergioborbone@gmail.com>
2022-02-14 22:25:43 +0100

    Aggiunto nome utente nella lsita chat e rispettiva propic tramite il v-for

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const app = new Vue({
+
+    el: '#app',
+    data: {
+
+        user: [
+        
+          {
+            name: 'Michele',
+            img: 'img/avatar_1.jpg',
+          },
+
+          {
+            name: 'Fabio',
+            img: 'img/avatar_2.jpg',
+          },
+
+          {
+            name: 'Samuele',
+            img: 'img/avatar_3.jpg',
+          },
+
+        ],
+
+    },
+
+})
