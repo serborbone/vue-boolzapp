e06eb269240bd95df491fc58358a2d795ef7bfc3
Sergio Borbone <sergioborbone@gmail.com>
2022-02-15 16:06:43 +0100

    Push

--- js/main.js
+++ js/main.js
@@ -5,45 +5,166 @@ const app = new Vue({
 
         user: [
         
-          {
-            name: 'Michele',
-            img: 'img/avatar_1.jpg',
-          },
+            {
+              name: 'Michele',
+              img: 'img/avatar_1.jpg',
+              messages: [
 
-          {
-            name: 'Fabio',
-            img: 'img/avatar_2.jpg',
-          },
+                  {            
+                      date: '10/01/2020 16:15:22',
+                      text: 'Messaggio1',
+                      status: 'sent',
+                  },
 
-          {
-            name: 'Samuele',
-            img: 'img/avatar_3.jpg',
-          },
+                    {            
+                      date: '10/01/2020 16:20:10',
+                      text: 'Messaggio2',
+                      status: 'received',
+                  },
 
-          {
-            name: 'Mario',
-            img: 'img/avatar_4.jpg',
-          },
+              ],
 
-          {
-            name: 'Riccardo',
-            img: 'img/avatar_5.jpg',
-          },
-
-          {
-            name: 'Alessia',
-            img: 'img/avatar_6.jpg',
-          },
-
-          {
-            name: 'Jack',
-            img: 'img/avatar_7.jpg',
-          },
+            },
 
             {
-            name: 'Mike',
-            img: 'img/avatar_8.jpg',
-          },
+              name: 'Fabio',
+              img: 'img/avatar_2.jpg',
+              messages: [
+
+                  {            
+                      date: '10/01/2020 16:21:22',
+                      text: 'Messaggio1',
+                      status: 'sent',
+                  },
+
+                    {            
+                      date: '10/01/2020 16:29:10',
+                      text: 'Messaggio Messaggio',
+                      status: 'received',
+                  },
+
+              ],
+            },
+
+            {
+              name: 'Samuele',
+              img: 'img/avatar_3.jpg',
+              messages: [
+
+                  {            
+                      date: '10/01/2020 16:15:22',
+                      text: 'Messaggio1',
+                      status: 'sent',
+                  },
+
+                    {            
+                      date: '10/01/2020 16:20:10',
+                      text: 'Messaggio2',
+                      status: 'received',
+                  },
+
+              ],
+            },
+
+            {
+              name: 'Mario',
+              img: 'img/avatar_4.jpg',
+              messages: [
+
+                  {            
+                      date: '10/01/2020 16:15:22',
+                      text: 'Messaggio1',
+                      status: 'sent',
+                  },
+
+                    {            
+                      date: '10/01/2020 16:20:10',
+                      text: 'Messaggio2',
+                      status: 'received',
+                  },
+
+              ],
+            },
+
+            {
+              name: 'Riccardo',
+              img: 'img/avatar_5.jpg',
+              messages: [
+
+                  {            
+                      date: '10/01/2020 16:15:22',
+                      text: 'Messaggio1',
+                      status: 'sent',
+                  },
+
+                    {            
+                      date: '10/01/2020 16:20:10',
+                      text: 'Messaggio2',
+                      status: 'received',
+                  },
+
+              ],
+            },
+
+            {
+              name: 'Alessia',
+              img: 'img/avatar_6.jpg',
+              messages: [
+
+                  {            
+                      date: '10/01/2020 16:15:22',
+                      text: 'Messaggio1',
+                      status: 'sent',
+                  },
+
+                    {            
+                      date: '10/01/2020 16:20:10',
+                      text: 'Messaggio2',
+                      status: 'received',
+                  },
+
+              ],
+            },
+
+            {
+              name: 'Jack',
+              img: 'img/avatar_7.jpg',
+              messages: [
+
+                  {            
+                      date: '10/01/2020 16:15:22',
+                      text: 'Messaggio1',
+                      status: 'sent',
+                  },
+
+                    {            
+                      date: '10/01/2020 16:20:10',
+                      text: 'Messaggio2',
+                      status: 'received',
+                  },
+
+              ],
+            },
+
+              {
+              name: 'Mike',
+              img: 'img/avatar_8.jpg',
+              messages: [
+
+                  {            
+                      date: '10/01/2020 16:15:22',
+                      text: 'Messaggio1',
+                      status: 'sent',
+                  },
+
+                    {            
+                      date: '10/01/2020 16:20:10',
+                      text: 'Messaggio2',
+                      status: 'received',
+                  },
+
+              ],
+            },
 
         ],
 
